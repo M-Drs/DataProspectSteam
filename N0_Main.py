47cb22df454dfed987624d4feff0a9c97d4aa720
M-Drs <martin.deyres@gmail.com>
2025-03-24 16:28:30 +0100

    maj

--- N0_Main.py
+++ N0_Main.py
@@ -1,12 +1,8 @@
 from N1_Call_API import get_steam_details, get_steam_review_score ; from N2_Insert_BDD import insert_games_in_database, insert_game_review_score
 import time, random
- 
-
-
 
 if __name__ == "__main__":
  
-
     i = 1
     while i < 99999:
         Resultat= get_steam_details(i)
@@ -14,15 +10,12 @@ if __name__ == "__main__":
         if "Error: API request failed" in Resultat:  # Check if 'Error' is in the return string
             time.sleep(random.uniform(10,15))
             i -= 1
-
         elif Resultat["Code_retour"] == False :
             insert_games_in_database(Resultat)
         elif Resultat["Code_retour"] == True :
-        # SEND THIS JSON Resultat["Full_reponse"]
             insert_games_in_database(Resultat)
             review_score_general = get_steam_review_score(i)
             insert_game_review_score(i, review_score_general)
-
         i += 1
         time.sleep(random.uniform(0.3,1))
 
