97d5e691c03b9f4c2f35e90f4ee4c12452e40d54
M-Drs <martin.deyres@gmail.com>
2025-06-18 16:43:28 +0200

    feature upload data to distant server

---
+++ N3_Data_Upload.py
@@ -0,0 +1,27 @@
+import subprocess, os
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+# Define source and destination
+load_dotenv()
+local_path = str(Path("steam_games_info.db").resolve())
+remote_user = os.getenv("REMOTE_USER")
+remote_host = os.getenv("REMOTE_HOST")
+remote_path = os.getenv("REMOTE_PATH")
+port = os.getenv("PORT")
+
+# Build scp command
+scp_command = [
+    "scp",
+    "-P", port,
+    local_path,
+    f"{remote_user}@{remote_host}:{remote_path}"
+]
+
+# Run it
+try:
+    subprocess.run(scp_command, check=True)
+    print("Transfer successful!")
+except subprocess.CalledProcessError as e:
+    print(f"SCP failed: {e}")
