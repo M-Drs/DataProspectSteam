1558d374a5d79ac045357ddd6692cf5b74e4d5e3
M-Drs <martin.deyres@gmail.com>
2025-06-21 09:32:07 +0200

    clean

--- N0_Main.py
+++ N0_Main.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
     i = last_max_id()
     while i < 9000:
         Resultat = get_steam_details(i)
+
         if "Error: API request failed" in Resultat:  
             time.sleep(random.uniform(10,15))
             i -= 1
-
         elif Resultat["Code_retour"] == False :
             insert_games_in_database(Resultat)
         elif Resultat["Code_retour"] == True :
             insert_games_in_database(Resultat)
-
             review_score_general = get_steam_review_score(i)
             insert_game_review_score(i, review_score_general)
+
         i += 1
         time.sleep(random.uniform(0.5,1))
 
@@ -25,4 +25,3 @@ if __name__ == "__main__":
         get_steam_all_reviews(APP_ID,steam_cursor)
 
     uploadToServer()
-    
