c454f8b05af4332af18ad2d7dfbdbbefa0801927
M-Drs <martin.deyres@gmail.com>
2025-11-16 03:11:16 +0100

    clean

--- N1_Call_API.py
+++ N1_Call_API.py
@@ -5,33 +5,22 @@ from N2_Insert_BDD import insert_review_in_database, insert_last_cursor_position
 def get_steam_details(APP_ID):
     
     APP_ID = str(APP_ID)
-    URL = f"https://store.steampowered.com/api/appdetails?appids={APP_ID}"
+    URL = f"https://store.steampowered.com/api/appdetails?appids={APP_ID}" 
 
     try : response = requests.get(URL) ; response.encoding = 'utf-8-sig'
     except Exception as e: input(f"Request failed for {APP_ID}: {e},press enter to retry") ; return None
 
-    print("\nAPP_ID:",APP_ID ,"Status:", response.status_code,"Content-Type:", response.headers.get("Content-Type"),"Raw text:", response.text[:50])
-
-    # if response.status_code != 200 or not response.text.strip() or  "json" not in content_type := response.headers.get("Content-Type", "").lower() :
     if response.status_code != 200 or not response.text.strip() or ("json" not in (response.headers.get("Content-Type", "").lower())):
-        print(f"Error for {APP_ID}")
+        print("Error APP_ID:",APP_ID,"Status:",response.status_code,"Content-Type:",response.headers.get("Content-Type"),"Raw text:", response.text[:50])
         return None
     
-    data = response.json()
-
+    data=response.json()
     if data[APP_ID]["success"] == False :
-        return {"Code_retour" : False,"APP_ID": APP_ID,"Game": None }
+        return {"APP_ID":APP_ID,"Game":None,"Code_retour":False }
     
-    info = data[APP_ID]["data"] ; name = info["name"] ;url_image = info["header_image"]
-    release_date = info["release_date"]["date"] ; price = info["price_overview"]["final_formatted"] if "price_overview" in info else None 
-    gratuit = info["is_free"] ; platforms = [k for k, v in info["platforms"].items() if v] ; genres = [genre["description"] for genre in info.get("genres",[])] ; detailed_description = info["detailed_description"]
-    supported_languages = info.get("supported_languages") ; metacritic_score = info.get("metacritic",{}).get("score") ; metacritic_url = info.get("metacritic",{}).get("url") 
-
-    print(f"\nGame: {name}\nRelease Date: {release_date}\nPrice: {price}\nGenres: {' | '.join(genres)}\nHeader_image: {url_image}\nAvailable on: {' | '.join(platforms)}\nmetacritic_score: {metacritic_score}\nmetacritic_url: {metacritic_url}")
-
-    return {"Code_retour" : True,"Full_reponse" : response,"Data":data,"APP_ID": APP_ID,"Game": name,"Release_Date": release_date,"Price": price,"Gratuit": gratuit,"Genres": ' | '.join(genres),"Detailed_description": detailed_description,
-            "Url_image": url_image,"Supported_languages": supported_languages,"Platforms": ' | '.join(platforms),"Metacritic_score": metacritic_score,"Metacritic_url": metacritic_url}
-
+    info=data[APP_ID]["data"] ; name=info["name"] ; url_image=info["header_image"] ; release_date=info["release_date"]["date"] ; price=info["price_overview"]["final_formatted"] if "price_overview" in info else None ; gratuit=info["is_free"] 
+    platforms=[k for k, v in info["platforms"].items() if v] ; genres=[genre["description"] for genre in info.get("genres",[])] ; detailed_description=info["detailed_description"] ; supported_languages=info.get("supported_languages") ; metacritic_score=info.get("metacritic",{}).get("score") ; metacritic_url=info.get("metacritic",{}).get("url") ; print(f"\nGame: {name}\nRelease Date: {release_date}\nPrice: {price}\nGenres: {' | '.join(genres)}\nHeader_image: {url_image}\nAvailable on: {' | '.join(platforms)}\nmetacritic_score: {metacritic_score}\nmetacritic_url: {metacritic_url}")
+    return {"Code_retour":True, "Full_reponse":response, "Data":data, "APP_ID":APP_ID,"Game":name, "Release_Date":release_date, "Price":price, "Gratuit":gratuit, "Genres":' | '.join(genres), "Detailed_description":detailed_description, "Url_image":url_image, "Supported_languages":supported_languages, "Platforms":' | '.join(platforms), "Metacritic_score":metacritic_score, "Metacritic_url":metacritic_url}
 
 def get_image(url_image):
     if url_image is not None :
